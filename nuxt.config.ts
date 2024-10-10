6b9b29a83d9c7d1999edac406f61a33fec97716d
Nathan Billis <n.billis@yusu.org>
2024-10-10 14:17:27 +0100

    add eslint and prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+
   nitro: {
     prerender: {
-      autoSubfolderIndex: false
-    }
-  }
-})
+      autoSubfolderIndex: false,
+    },
+  },
+  modules: ["@nuxt/eslint"],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+});
