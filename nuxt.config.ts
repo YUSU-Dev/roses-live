7b5605a1683ac74d2c8a576efc7eb9104bd3d953
Nathan Billis <n.billis@yusu.org>
2024-12-09 16:51:55 +0000

    fix eslint and add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
   nitro: {
@@ -8,27 +8,27 @@ export default defineNuxtConfig({
       autoSubfolderIndex: false,
     },
   },
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/eslint', '@sentry/nuxt/module', '@nuxtjs/tailwindcss'],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@sentry/nuxt/module", "@nuxtjs/tailwindcss"],
   eslint: {
     config: {
-      stylistic: true,
+      stylistic: false,
     },
   },
   app: {
     head: {
       link: [
         {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/roses-logo.png',
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/roses-logo.png",
         },
-        { rel: 'stylesheet', href: 'https://use.typekit.net/nks8xpu.css' },
+        { rel: "stylesheet", href: "https://use.typekit.net/nks8xpu.css" },
         {
-          rel: 'stylesheet',
-          href: 'https://use.fontawesome.com/releases/v6.7.1/css/all.css',
+          rel: "stylesheet",
+          href: "https://use.fontawesome.com/releases/v6.7.1/css/all.css",
         },
       ],
     },
   },
-})
+});
