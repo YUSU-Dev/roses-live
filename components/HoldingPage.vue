39583d19e0612eceb66c29dd58bd3161ba9a0901
danrparker <95368712+danrparker@users.noreply.github.com>
2024-12-09 12:22:29 +0000

    Holding page tweaks (#47)
    * Holding page tweaks
    
    * Fixed font on Safari, added correct favicon

--- components/HoldingPage.vue
+++ components/HoldingPage.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex flex-wrap justify-center items-center gap-y-8">
-    <img
-      src="https://assets-cdn.sums.su/YU/website/img/Roses/500x500_Roses_Holding_Page.webp"
-      class="aspect-square"
-      alt="Roses 2025 logo"
-    >
+  <div class="flex flex-wrap justify-center items-center h-full">
+    <div class="max-w-[500px] max-h-[500px]">
+      <img
+        src="https://assets-cdn.sums.su/YU/website/img/Roses/500x500_Roses_Holding_Page.webp"
+        class="aspect-square w-full object-contain"
+        alt="Roses 2025 logo"
+      >
+    </div>
     <div class="flex flex-col gap-y-8 text-center">
       <h1 class="text-5xl xxs:text-6xl sm:text-8xl">
         #RosesAreWhite
@@ -13,7 +15,7 @@
       <div class="flex justify-center">
         <SocialLinks />
       </div>
-      <div class="flex justify-center items-center gap-6">
+      <div class="flex justify-center items-center gap-6 h-20">
         <a href="https://yorksu.org/"><img
           src="https://assets-cdn.sums.su/YU/website/img/yorksu-logo-black-full.png"
           alt="University of York Students' Union"
