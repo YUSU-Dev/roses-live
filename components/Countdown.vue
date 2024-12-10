bd1b450bf0503edd3f3575fd367652f5cf8df5ae
Nathan Billis <nathanbillis@users.noreply.github.com>
2024-12-10 10:17:38 +0000

    Merge dev into main (#50)
    * Holding page (#43)
    
    * Dparker/ros 34 (#45)
    
    * Holding page
    
    * Style Fix
    
    ---------
    
    Signed-off-by: Nathan Billis <nathanbillis@users.noreply.github.com>
    Co-authored-by: Nathan Billis <nathanbillis@users.noreply.github.com>
    
    * Holding page tweaks (#47)
    
    * Holding page tweaks
    
    * Fixed font on Safari, added correct favicon
    
    * fix eslint and add meta tags (#49)
    
    ---------
    
    Signed-off-by: Nathan Billis <nathanbillis@users.noreply.github.com>
    Co-authored-by: danrparker <95368712+danrparker@users.noreply.github.com>

--- components/Countdown.vue
+++ components/Countdown.vue
@@ -6,9 +6,7 @@
           {{ time.days }}
         </p>
       </div>
-      <p class="countdown-title">
-        DAYS
-      </p>
+      <p class="countdown-title">DAYS</p>
     </div>
     <div class="flex flex-col items-center gap-4">
       <div class="countdown-tile">
@@ -16,9 +14,7 @@
           {{ time.hours }}
         </p>
       </div>
-      <p class="countdown-title">
-        HOURS
-      </p>
+      <p class="countdown-title">HOURS</p>
     </div>
     <div class="flex flex-col items-center gap-4">
       <div class="countdown-tile">
@@ -26,9 +22,7 @@
           {{ time.minutes }}
         </p>
       </div>
-      <p class="countdown-title">
-        MINUTES
-      </p>
+      <p class="countdown-title">MINUTES</p>
     </div>
     <div class="flex flex-col items-center gap-4">
       <div class="countdown-tile">
@@ -36,16 +30,14 @@
           {{ time.seconds }}
         </p>
       </div>
-      <p class="countdown-title">
-        SECONDS
-      </p>
+      <p class="countdown-title">SECONDS</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'RosesCountdown',
+  name: "RosesCountdown",
   props: {
     date: {
       type: String,
@@ -60,34 +52,34 @@ export default {
         minutes: 0,
         seconds: 0,
       },
-    }
+    };
   },
   mounted() {
-    this.countdown()
+    this.countdown();
   },
   methods: {
     countdown() {
-      const countDownDate = new Date(this.date).getTime()
+      const countDownDate = new Date(this.date).getTime();
       const x = setInterval(() => {
-        const now = new Date().getTime()
-        const distance = countDownDate - now
-        this.time.days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+        this.time.days = Math.floor(distance / (1000 * 60 * 60 * 24));
         this.time.hours = Math.floor(
           (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-        )
+        );
         this.time.minutes = Math.floor(
           (distance % (1000 * 60 * 60)) / (1000 * 60),
-        )
-        this.time.seconds = Math.floor((distance % (1000 * 60)) / 1000)
+        );
+        this.time.seconds = Math.floor((distance % (1000 * 60)) / 1000);
         if (distance < 0) {
-          clearInterval(x)
-          this.time.days = 0
-          this.time.hours = 0
-          this.time.minutes = 0
-          this.time.seconds = 0
+          clearInterval(x);
+          this.time.days = 0;
+          this.time.hours = 0;
+          this.time.minutes = 0;
+          this.time.seconds = 0;
         }
-      }, 1000)
+      }, 1000);
     },
   },
-}
+};
 </script>
