34da344c068f6cf829e380cf66cafb455cbcabbb
Nathan Billis <n.billis@yorksu.org>
2025-04-25 15:33:54 +0100

    revert sponsors

--- components/SponsorsBanner.vue
+++ components/SponsorsBanner.vue
@@ -6,40 +6,32 @@
       <h2 class="text-5xl xcond font-bold text-center">PROUDLY SUPPORTED BY</h2>
       <div class="flex flex-col lg:flex-row justify-center items-center gap-6">
         <a href="https://go.yorksu.org/4GpEEUC" target="_blank">
-          <NuxtImg
-            height="96px"
-            width="100%"
-            fit="contain"
+          <img
+            class="h-24"
             src="https://assets-cdn.sums.su/YU/website/img/Roses/LUSU_Logo.png"
             alt="Lancaster Students' Union"
             loading="lazy"
           />
         </a>
         <a href="https://go.yorksu.org/9ZR9WTA" target="_blank">
-          <NuxtImg
-            height="96px"
-            width="100%"
-            fit="contain"
+          <img
+            class="h-24"
             src="https://assets-cdn.sums.su/YU/website/img/Roses/YS-logo-BLACK.png"
             alt="York Sport"
             loading="lazy"
           />
         </a>
         <a href="https://go.yorksu.org/oujNo29" target="_blank">
-          <NuxtImg
-            height="96px"
-            width="100%"
-            fit="contain"
+          <img
+            class="h-24"
             src="https://assets-cdn.sums.su/YU/website/img/Roses/equitix_logo.png"
             alt="Equitix"
             loading="lazy"
           />
         </a>
         <a href="https://go.yorksu.org/zQ85tmg" target="_blank">
-          <NuxtImg
-            height="96px"
-            width="100%"
-            fit="contain"
+          <img
+            class="h-24"
             src="https://assets-cdn.sums.su/YU/website/img/Roses/Uni_of_York_logo.png"
             alt="University of York"
             loading="lazy"
